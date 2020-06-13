2e2809477e5a699f399f5a33f7e96d048fa152c1
root <root@DESKTOP-SMUCDAM.localdomain>
2020-06-12 22:02:19 -0700

    sys module done

--- pyProj/pyt.py
+++ pyProj/pyt.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
-name = "Abdelrahman"
-print(name)
+import sys
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+fav_color = sys.argv[4]
+print(name, "+", color,"+", activity,"+", fav_color)
+
+
